b13c62f7c4b414a8db6e484dd10ebbfb3003ccf3
Matthew Volk <volkmattj@gmail.com>
2024-09-09 17:32:00 -0500

    refactor: use node 22 (#1)

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,17 @@
-import eslint from "@eslint/js";
+import globals from "globals";
+import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-export default tseslint.config(
-  eslint.configs.recommended,
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
-);
+  {
+    rules: {
+      "prettier/prettier": "warn",
+    },
+  },
+];
