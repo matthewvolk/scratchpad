5103f89398916f3188cfa4ee80bdcf204710d71e
Matthew Volk <volkmattj@gmail.com>
2024-11-05 14:39:12 -0600

    chore: add lint script and fix lint errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import eslintPluginPrettierRecommended  from 'eslint-plugin-prettier/recommended';
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   eslintPluginPrettierRecommended,
 ];
