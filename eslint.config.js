974eaf56afc5726013e6e00e9001a91af5ff8238
Matthew Volk <volkmattj@gmail.com>
2024-11-05 14:41:50 -0600

    fix: configure environment as node.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   eslintPluginPrettierRecommended,
 ];
