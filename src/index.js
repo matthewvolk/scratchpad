64ac340c7f24127d4406812d61115561457fb399
Matthew Volk <volkmattj@gmail.com>
2024-11-06 17:56:08 -0600

    chore: rename package

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import { program } from "@commander-js/extra-typings";
 
 import { catalog } from "./commands/catalog.js";
 
-program.name("cli").addCommand(catalog);
+program.name("scratchpad").addCommand(catalog);
 
 program.parse(process.argv);
